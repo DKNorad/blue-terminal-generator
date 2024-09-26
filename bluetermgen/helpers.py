7f2db33df23da0ba3ba0fa8e1a2b8a58c14a8c6f
DKNorad <nedd.petrov@gmail.com>
2024-09-26 23:36:46 +0300

    New help function for the table element.

--- bluetermgen/helpers.py
+++ bluetermgen/helpers.py
@@ -12,14 +12,19 @@ def calculate_inner_width(
     Args:
         header (list):
             The header to calculate the width for.
+
         footer (str, optional):
-            The footer to calculate the width for. Defaults to None.
+            The footer to calculate the width for. Defaults to `None`.
+
         options (list, optional):
-            The menu options to calculate the width for. Defaults to None.
+            The menu options to calculate the width for. Defaults to `None`.
+
         min_width (int, optional):
-            The minimum width of the item. Defaults to 0.
+            The minimum width of the item. Defaults to `0`.
+
         padx (tuple, optional):
-            Padding to add around the header, footer, and options. Defaults to 0.
+            Padding to add around the `header`, `footer`, and `options`.
+            Defaults to `(0, 0)`.
             - For message:
             (a, b) = (left, right)
 
@@ -70,6 +75,98 @@ def calculate_inner_width(
     return width
 
 
+def calculate_table_inner_width(
+    table_data: list,
+    headers: list,
+    padx: tuple,
+    minimum_width: int | dict,
+    is_dict_table: bool,
+    indexing: bool,
+) -> dict:
+    """
+    Calculates the width for each column in the `table_data`,
+    optionally applying padding.
+
+    Args:
+        table_data (list):
+            The table data to calculate the width for.
+            It can be a list of dictionaries or a list of lists.
+
+            Examples:
+                [
+                    ["header 1", "header 2"],
+                    ["row 1, col 1", "row 1, col 2"],
+                    ["row 2, col 1", "row 2, col 2"],
+                ]
+
+                [
+                    {"header 1": ["row 1, col 1", "row 1, col 2"]},
+                    {"header 2": ["row 2, col 1", "row 2, col 2"]},
+                ]
+
+        headers (list, optional):
+            The table headers.
+
+        padx (tuple, optional):
+            Padding to add around the table.
+
+        minimum_width (int | dict, optional):
+            The minimum width for all the columns or for each column.
+
+        is_dict_table (bool, optional):
+            Whether the table is a dictionary table.
+
+        indexing (bool, optional):
+            Whether the table has an index column.
+
+    Returns:
+        dict:
+            The width for each column in the `'table_data'` and
+            the updated `'table_data'` if padding is applied.
+
+            Example:
+            {col1: width, col2: width, ...}
+    """
+
+    column_widths = {}
+    header_padx, data_padx = padx
+    starting_index = 0
+
+    # Calculate the number of rows to get the index width if set.
+    if indexing:
+        index_width = len(str(len(table_data)))
+        column_widths[0] = index_width
+        starting_index = 1
+
+    if headers != "None":
+        for col_i, header in enumerate(headers, starting_index):
+            column_widths[col_i] = max(
+                len(header) + sum(header_padx), minimum_width)
+
+    if is_dict_table:
+        for row in table_data:
+            for col_i, value in enumerate(row.values(), starting_index):
+                col_width = len(str(value)) + sum(data_padx)
+                if col_i not in column_widths:
+                    column_widths[col_i] = max(col_width, minimum_width)
+                else:
+                    column_widths[col_i] = max(
+                        column_widths[col_i], col_width, minimum_width)
+
+    else:
+        for row in table_data:
+            for col_index, col_value in enumerate(row, starting_index):
+                col_width = len(str(col_value)) + sum(data_padx)
+                col_width = max(col_width, minimum_width)
+                if col_index not in column_widths:
+                    column_widths[col_index] = col_width
+                else:
+                    column_widths[col_index] = max(
+                        column_widths[col_index], col_width, minimum_width)
+
+    return column_widths
+
+
 """ Styles legend:
 tl - top left
 tr - top right
@@ -82,7 +179,9 @@ ml - middle left
 mr - middle right
 mt - middle top
 mb - middle bottom
+hb - header bottom
 """
+
 DOUBLE_STYLE = {
     "tl": "╔",
     "tr": "╗",
@@ -95,6 +194,7 @@ DOUBLE_STYLE = {
     "mr": "╣",
     "mt": "╦",
     "mb": "╩",
+    "hb": "═",
 }
 SINGLE_STYLE = {
     "tl": "┌",
@@ -108,6 +208,7 @@ SINGLE_STYLE = {
     "mr": "┤",
     "mt": "┬",
     "mb": "┴",
+    "hb": "─",
 }
 ASCII_STYLE = {
     "tl": "+",
@@ -117,10 +218,11 @@ ASCII_STYLE = {
     "h": "-",
     "v": "|",
     "c": "+",
-    "ml": "+",
-    "mr": "+",
+    "ml": "|",
+    "mr": "|",
     "mt": "+",
     "mb": "+",
+    "hb": "=",
 }
 
 STYLES = {
